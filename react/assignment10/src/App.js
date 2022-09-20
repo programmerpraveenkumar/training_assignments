f5c1da958de65345894886f6ff8f78c94598f446
amandalim5 <113882127+amandalim5@users.noreply.github.com>
2022-09-20 17:50:23 +0800

    assignment 10 commit

---
+++ react/assignment10/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Assignment Number 10</h1>
+      <p>The space for a paragraph.</p>
+    </div>
+  );
+}
+
+export default App;
